82ff9173c36fbe0ed59cc5f228a50ca14cdc5b42
LesterAnthony <lesteranthonyjr@gmail.com>
2023-07-26 00:45:18 +0800

    Program functionality

--- Graph.java
+++ Graph.java
@@ -1,35 +1,40 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
-    ArrayList<ArrayList<Integer>> graph;
-    int count;
 
-    Graph(int count) {
+    private ArrayList<ArrayList<Integer>> graph;
+    private int users;
+
+    public Graph(int users) {
+        this.users = users;
         int i;
-        this.count = count;
-
 
         graph = new ArrayList<ArrayList<Integer>>();
 
-        for(i = 0; i < count; i++) {
+        for(i = 0; i < users; i++) {
             graph.add(new ArrayList<Integer>());
         }
     }
 
-    void addConnection(int user1, int user2) {
+    public ArrayList<ArrayList<Integer>> getGraph() {
+        return graph;
+    }
+
+    public void addConnection(int user1, int user2) {
         graph.get(user1).add(user2);
         graph.get(user2).add(user1);
     }
 
-    void getFriendList(int user) {
-        // IMPLEMENT
+    public void displayFriendList(int user) {
         int i;
         int n = (graph.get(user)).size();
         int tempFriend;
         boolean userFound = true;
 
         // Displays error if user is not found
-        if(user < 0 || user > count-1) {
+        if(user < 0 || user > users-1) {
             System.out.println("ERROR: User does not exist.");
             userFound = false;
         }
@@ -42,8 +47,69 @@ public class Graph {
                 tempFriend = (graph.get(user)).get(i);
                 System.out.print(tempFriend + " ");
             }
+            System.out.println("\n");
         }
         
+    }
+
+    public void displayConnections(ArrayList<ArrayList<Integer>> graph, int userSrc, int userDst, int nodes) {
+        int prevConnections[] = new int[nodes];
+        int distConnections[] = new int[nodes];
+
+        if(shortestPath(graph, userSrc, userDst, nodes, prevConnections, distConnections) == false) {
+            System.out.println("Cannot find connection between " + userSrc + " and " + userDst);
+            return;
+        }
+
+        LinkedList<Integer> path = new LinkedList<Integer>();
+        int crawl = userDst;
+        path.add(crawl);
+
+        while(prevConnections[crawl] != -1) {
+            path.add(prevConnections[crawl]);
+            crawl = prevConnections[crawl];
+        }
+
+        
+        System.out.println("There is a connection from " + userSrc + " to " + userDst + "!");
+        for(int i = path.size() - 1; i >= 0; i--) {
+            if(i > 0)
+                System.out.println(path.get(i) + " is friends with " + path.get(i-1));
+        }
+        System.out.println();
+
+    }
+
+    public boolean shortestPath(ArrayList<ArrayList<Integer>> graph, int userSrc, int userDst, int nodes, int prevConnections[], int distConnections[]) {
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        boolean visited[] = new boolean[nodes];
+
+        for(int i = 0; i < nodes; i++) {
+            visited[i] = false;
+            distConnections[i] = Integer.MAX_VALUE;
+            prevConnections[i] = -1;
+        }
+
+        visited[userSrc] = true;
+        distConnections[userSrc] = 0;
+        queue.add(userSrc);
+
+        while(!queue.isEmpty()) {
+            int u = queue.remove();
+            for(int i = 0; i < graph.get(u).size(); i++) {
+                if(visited[graph.get(u).get(i)] == false) {
+                    visited[graph.get(u).get(i)] = true;
+                    distConnections[graph.get(u).get(i)] = distConnections[u] + 1;
+                    prevConnections[graph.get(u).get(i)] = u;
+                    queue.add(graph.get(u).get(i));
+
+                    if(graph.get(u).get(i) == userDst)
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
